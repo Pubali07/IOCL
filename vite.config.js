02e2305787895269629a295f592e03db8f7b2689
Pubali07 <147859899+Pubali07@users.noreply.github.com>
2026-01-15 12:46:43 +0530

    Fix Vercel blank page (remove base path)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/iocl-website/',
-});
+  plugins: [react()]
+})
